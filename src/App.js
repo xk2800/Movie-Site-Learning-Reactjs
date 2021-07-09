cfaaa6b87ab8732941da29d62026ca98a494cf87
Xavier K <xavierkhew00@gmail.com>
2021-07-10 01:59:09 +0800

    Global Styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+//import React from 'react';
+
+//styles import from GlobalStyle.js
+import {GlobalStyle} from './GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
