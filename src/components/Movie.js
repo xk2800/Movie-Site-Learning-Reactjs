faf5daa820b8e89ac57f5f83b31c41c5e0daa4a9
Xavier K <xavierkhew00@gmail.com>
2021-07-10 19:44:31 +0800

    Movie Component - Scaffold

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,3 +1,25 @@
-const Movie = () => <div>Movie</div>;
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE} from '../config';
+
+//components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+//hook
+
+//image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+
+    return(
+        <>
+            <div>Movie</div>
+
+
+        </>
+    )
+
+};
 
 export default Movie;
